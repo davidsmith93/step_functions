7f90d3a7b2e29bdc71578f4e6df8d159d89dedc2
davidsmith93 <davidsmith@rentalcars.com>
2020-05-01 09:16:36 +0100

    first commit

---
+++ example_1/hello_lambda/python3_8/handler.py
@@ -0,0 +1,9 @@
+import json
+
+
+def lambda_handler(event, context):
+    event_obj = event if not isinstance(event, str) else json.load(event)
+
+    return {
+        "message": f"Hello {event_obj['Input']['name']}!"
+    }
